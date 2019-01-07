f9df0d6dbe7bb1001ccc2d9068b53df751762590
Andri Deng <andri.deng@bvolo.com>
2019-01-07 07:33:25 +0700

    add wallet and transaction together with the unit test

---
+++ wallet/transaction.test.js
@@ -0,0 +1,39 @@
+const Transaction = require('./transaction');
+const Wallet = require('./index');
+
+describe('Transaction', () => {
+    let transaction, wallet, recipient, amount;
+
+    beforeEach(() => {
+        wallet = new Wallet();
+        amount = 50;
+        recipient = 'r3c1p13n7';
+        transaction = Transaction.newTransaction(wallet, recipient, amount);
+    });
+
+    it('outputs the `amount` subtracted from the wallet balance', () => {
+        expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+            .toEqual(wallet.balance - amount);
+    });
+
+    it('outputs the `amount` added to the recipient', () => {
+        expect(transaction.outputs.find(output => output.address === recipient).amount)
+            .toEqual(amount);
+    });
+
+    it('inputs the balance of the wallet', () => {
+        console.log(transaction.input);
+        expect(transaction.input.amount).toEqual(wallet.balance);
+    });
+
+    describe('transaction with an amount that exceeds the balance', () => {
+        beforeEach(() => {
+            amount = 50000;
+            transaction = Transaction.newTransaction(wallet, recipient, amount);
+        });
+        
+        it('does not create the transaction', () => {
+            expect(transaction).toEqual(undefined);
+        });
+    });
+});
