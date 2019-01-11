1f3fc2eeb7b2e368d0511d81f10eb77bccc59db1
Andri Deng <andri.deng@bvolo.com>
2019-01-11 07:06:27 +0700

    connect p2p server and get public-key

--- app/index.js
+++ app/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
-const p2pServer = new P2pServer(bc);
+const wallet = new Wallet();
+const tp = new TransactionPool();
+const p2pServer = new P2pServer(bc, tp);
 
 // - can receive json
 app.use(bodyParser.json());
@@ -27,6 +31,22 @@ app.post('/mine', (req, res) => {
     p2pServer.syncChains();
 
     res.redirect('/blocks');
+});
+
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
+});
+
+app.post('/transact', (req, res) => {
+    const { recipient, amount } = req.body;
+    // - create new transaction
+    const transaction = wallet.createTransaction(recipient, amount, tp);
+    p2pServer.broadcastTransaction(transaction);
+    res.redirect('/transactions');
+});
+
+app.get('/public-key', (req, res) => {
+    res.json({ publicKey: wallet.publicKey });
 });
 
 app.listen(HTTP_PORT, () => { console.log(`listening on port: ${HTTP_PORT}`); });
