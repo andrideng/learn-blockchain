6d610aac5273c6d99a32d360ef8d735370c9a9c3
Andri Deng <andri.deng@bvolo.com>
2019-01-13 09:43:58 +0700

    update transaction p2p balance

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -1,14 +1,16 @@
 const TransactionPool = require('./transaction-pool');
 const Transaction = require('./transaction');
 const Wallet = require('./index');
+const Blockchain = require('../blockchain');
 
 describe('TransactionPool', () => {
-    let tp, wallet, transaction;
+    let tp, wallet, transaction, bc;
 
     beforeEach(() => {
         tp = new TransactionPool();
         wallet = new Wallet();
-        transaction = wallet.createTransaction('r4nd-4ddr355', 30, tp);
+        bc = new Blockchain();
+        transaction = wallet.createTransaction('r4nd-4ddr355', 30, bc, tp);
     });
 
     it('adds a transactions to the pool', () => {
@@ -25,13 +27,18 @@ describe('TransactionPool', () => {
             .not.toEqual(oldTransaction);
     });
 
+    it('clears transactions', () => {
+        tp.clear();
+        expect(tp.transactions).toEqual([]);
+    });
+
     describe('mixing valid and corrupt transactions', () => {
         let validTransactions;
         beforeEach(() => {
             validTransactions = [...tp.transactions];
             for (let i = 0; i < 6; i++) {
                 wallet = new Wallet();
-                transaction = wallet.createTransaction('r4nd-4ddr355', 30, tp);
+                transaction = wallet.createTransaction('r4nd-4ddr355', 30, bc, tp);
                 if (i % 2 === 0) {
                     transaction.input.amount = 99999;
                 } else {
